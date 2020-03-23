2890d63605cbc1ae55b7b17365d251f624721f20
BrunoGino <bruno.gino24@gmail.com>
2020-03-23 16:06:18 -0300

    Updated colors, fixed navigation and updated main icon

--- app/src/main/java/br/com/horizon/MainActivity.java
+++ app/src/main/java/br/com/horizon/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         // menu should be considered as top level destinations.
 
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_main_list, R.id.navigation_favorites, R.id.navigation_wallet)
                 .build();
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
